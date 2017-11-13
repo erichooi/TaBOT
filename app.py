fb98bcb17b52de83a040a86d4259ccc434be8715
Eric Khoo Jiun Hooi <erichooi1995@gmail.com>
2017-11-13 21:37:27 +0800

    test 23

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def webhook():
         recipient_id = message_content["recipient"]["id"]
 
         if ("message" in message_content.keys() and "is_echo" not in message_content.keys()):
+            print(message_content)
             message = message_content["message"]["text"]
-            print(message)
             tabot.generate_answer_type(message)
             answer_type = tabot.get_answer_type()
             if answer_type == "event_only":
