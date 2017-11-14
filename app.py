1c245a6814dce40b52fd048f6cc13a51654a151f
Eric Khoo Jiun Hooi <erichooi1995@gmail.com>
2017-11-14 16:23:22 +0800

    test 58

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def webhook():
             else:
                 send_text_message(facebook_access_token, sender_id, help_text)
         else:
-            pass
+            send_text_message(facebook_access_token, sender_id, help_text)
     else:
         pass
 
